b579acb125e35829d7e7df37d1c05a5ca5cff673
Souhail Merroun <souhail.merroun1996@gmail.com>
2022-04-23 21:14:22 +0200

    Displaying list of characters from graphql query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,54 @@
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: 'https://rickandmortyapi.com/graphql',
+    cache: new InMemoryCache(),
+  })
+
+  const { data } = await client.query({
+    query: gql`
+      query {
+        characters(page: 1) {
+          info {
+            count
+            pages
+            next
+            prev
+          }
+          results {
+            id
+            name
+            image
+          }
+        }
+      }
+    `
+  });
+
+  const characters = data.characters.results;
+  const info = data.characters.info;
+
+  return {
+    props: {
+      characters,
+      info
+    },
+  }
+}
+
+interface Props {
+  characters: any
+  info: any
+}
+
+const Home = ({ characters, info }: Props) => {
+  console.log(characters)
+  console.log(info);
   return (
     <div className={styles.container}>
       <Head>
@@ -12,59 +57,29 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <h1 className={styles.title}>
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
+      <hr />
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+      <ul>
+        <li>{info.count} characters</li>
+        <li>{info.pages} pages</li>
+      </ul>
+      <button disabled={info.prev === null} onClick={() => console.log('previous')}>Previous</button>
+      <button disabled={info.next === null} onClick={() => console.log('next')}>Next</button>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+      <hr />
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      {
+        characters.map((character: any) => (
+          <div key={character.id}>
+            <img src={character.image} />
+            <h2>{character.name}</h2>
+          </div>
+        ))
+      }
     </div>
   )
 }
